8ef84c28f3b4ceab05b260269634b85fd35a799e
NirajFonseka <fonseka.live@utexas.edu>
2019-03-09 01:54:10 -0600

    Updated the proto files and cleaned up more things

--- proto-build.sh
+++ proto-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-protoc --proto_path=api/proto/v1 -I${GOPATH}/src --go_out=plugins=grpc:pkg/api/v1 api/proto/v1/api.proto
+protoc --proto_path=api/proto/v1 -I${GOPATH}/src --go_out=plugins=grpc:pkg/api/v1 api/proto/v1/*
